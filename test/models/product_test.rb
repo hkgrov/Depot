4cf2a11c8115a72ddd0e4e949cd4cc03c88796e7
EC2 Default User <ec2-user@ip-172-31-16-152.us-east-2.compute.internal>
2018-07-23 14:19:34 +0000

    Added validation and some test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+  
+  test "price must be positive" do
+    product = Product.new(title: "My litle pony", description: "Another fluffy animal just waiting to be hugged. It smells like candy and is twice as addictive", image_url: "MyLitlePony.PNG")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    
+    product.price = 1
+    assert product.valid?
+  end
+  
+  def new_product(image_url)
+    Product.new(title: "Bjarne", description: "A small but very capable pug huggable animal. It might be small, but what it lacks in size it makes up for in pure cuteness", price: 1, image_url: image_url)
+  end
+  
+  test "Image_url" do
+    ok = %w{fred.gif fred.jpg fred.png fred.JPG FRED.jpg http://a.b.c/x/y/z/fred.gif}
+    bad = %w{fred.doc fred.gif/more fred.gif.more}
+    
+    ok.each do |image_url|
+      assert new_product(image_url).valid?, "#{image_url} shouldn‘t be invalid" 
+    end
+    
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?, "#{image_url} shouldn't be valid"
+    end
+    
 end
+end
+
