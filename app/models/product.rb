286320296a19e4762ba455cae553dd17e3561c34
EC2 Default User <ec2-user@ip-172-31-16-152.us-east-2.compute.internal>
2018-07-24 16:34:23 +0000

    Started working on a shopping cart and added a button to add to cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,19 @@
 class Product < ApplicationRecord
+    has_many :line_items
     
-    
+    before_destroy :ensure_not_referenced_by_any_line_item
     validates :description, :image_url, presence: true
     validates :price, numericality: {greater_than_or_equal_to: 0.01}
     validates :title, uniqueness: true
     validates :image_url, allow_blank: true, format: {with: %r{\.(gif|png|jpg)\Z}i, message: 'must be a URL for a GIF, PNG or JPG image'}
-    
     validates :title, length: {minimum: 10, message: "must be at least 10 characters long"}
+    
+    private
+        def ensure_not_referenced_by_any_line_item
+            unless line_items.empty?
+                error.add(:base, 'Line Items present')
+                throw :abort
+            end
+        end
+    
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -38,7 +38,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
     assert_redirected_to product_url(@product)
   end
-
+  
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
@@ -46,4 +46,22 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+  
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+    
+    assert_redirected_to products_url
+  end
+  
+  test "Should destroy product" do
+    assert_difference('Product.count', 1) do
+      delete product_url(@product)
+    end
+    assert_redirected_to products_url
+  end
+  
+  
 end
+
