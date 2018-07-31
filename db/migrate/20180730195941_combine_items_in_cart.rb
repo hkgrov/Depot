86effaa66f858ac01ecc53f03406f30b55add748
EC2 Default User <ec2-user@ip-172-31-16-152.us-east-2.compute.internal>
2018-07-31 10:28:47 +0000

    Added a down function to the migration and fixed some bugs

--- db/migrate/20180730195941_combine_items_in_cart.rb
+++ db/migrate/20180730195941_combine_items_in_cart.rb
@@ -11,16 +11,17 @@ class CombineItemsInCart < ActiveRecord::Migration[5.2]
           end
         end
       end
-  end
+
   
   def down
     LineItem.where("quantity>1").each do |line_item|
       line_item.quantity.times do
-        LineItem.create(cart_id: line_item.cart_id
-                        product_id: line_item.product_id
+        LineItem.create(cart_id: line_item.cart_id,
+                        product_id: line_item.product_id,
                         quantity: 1)
-        end
+      end
         
-        line_item.destroy              
+        line_item.destroy 
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_30_195941) do
+ActiveRecord::Schema.define(version: 2018_07_27_120521) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
